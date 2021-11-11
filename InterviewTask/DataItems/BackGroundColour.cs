d325a009539ffdf28b3bbca982a3b81b2ddf705a
riazosmani <48535893+riazosmani@users.noreply.github.com>
2021-11-11 21:10:47 +0000

    Submission For Review

---
+++ InterviewTask/DataItems/BackGroundColour.cs
@@ -0,0 +1,8 @@
+﻿namespace InterviewTask.DataItems
+{
+    public static class BackgroundColour
+    {
+        public static string Grey = "bg-color-light-grey";
+        public static string Orange = "bg-color-donation-orange";
+    }
+}
---
+++ InterviewTask/DataItems/MessageAndColour.cs
@@ -0,0 +1,8 @@
+﻿namespace InterviewTask.DataItems
+{
+    public class MessageAndColour
+    {
+        public string Message { get; set; }
+        public string BackgroundColour { get; set; }
+    }
+}
