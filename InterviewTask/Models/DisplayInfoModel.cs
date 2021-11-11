34ac41ecca1d8926a3c4a19900d706e0df407a1b
riazosmani <48535893+riazosmani@users.noreply.github.com>
2021-11-11 21:13:32 +0000

    Submission For Review

---
+++ InterviewTask/Models/DisplayInfoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace InterviewTask.Models
+{
+    public class DisplayInfoModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string OpeningTimeMessage { get; set; }
+        public string BackgroundColour { get; set; }
+        public string WeatherLocation { get; set; }
+    }
+}
---
+++ InterviewTask/Models/WeatherModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InterviewTask.Models
+{
+    public class WeatherModel
+    {
+        public string City { get; set; }
+        public string Description { get; set; }
+        public float Temperature { get; set; }
+        public int Humidity { get; set; }
+    }
+}
